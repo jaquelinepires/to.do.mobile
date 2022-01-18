2505ca300ba846319686f804d6175a710e0cb716
jaquelinepires <piresjaqueline17@gmail.com>
2022-01-18 23:40:13 +0000

    add check and uncheck functionality

--- src/components/TasksList.tsx
+++ src/components/TasksList.tsx
@@ -33,11 +33,11 @@ export function TasksList({ tasks, toggleTaskDone, removeTask }: TasksListProps)
                 testID={`button-${index}`}
                 activeOpacity={0.7}
                 style={styles.taskButton}
-                //TODO - use onPress (toggle task) prop
+                onPress={() => toggleTaskDone(item.id)} 
               >
                 <View 
                   testID={`marker-${index}`}
-                  //TODO - use style prop 
+                  style={item.done ? styles.taskMarkerDone : styles.taskMarker}
                 >
                   { item.done && (
                     <Icon 
@@ -49,7 +49,7 @@ export function TasksList({ tasks, toggleTaskDone, removeTask }: TasksListProps)
                 </View>
 
                 <Text 
-                  //TODO - use style prop
+                  style={item.done ? styles.taskTextDone : styles.taskText}
                 >
                   {item.title}
                 </Text>
@@ -95,7 +95,7 @@ const styles = StyleSheet.create({
     justifyContent: 'center'
   },
   taskText: {
-    color: '#666',
+    color: 'white',
     fontFamily: 'Inter-Medium'
   },
   taskMarkerDone: {
